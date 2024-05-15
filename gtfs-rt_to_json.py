8a66c9ea425545f91297e76339249404280bf704
max-bld <mblanchard95@gmail.com>
2024-05-15 11:42:05 +0200

    app

---
+++ gtfs-rt_to_json.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 11:05:39 2024
+
+@author: maximeb
+"""
+
+import streamlit as st
+import os
+from time import sleep
+import json
+
+cwd = os.getcwd()
+output_file = 'gtfs-rt_converted.json'
+
+st.set_page_config(layout="wide")
+st.title('GTFS-RT to JSON')
+
+while True:
+    os.system(f'gtfs-realtime https://pysae.com/api/v2/groups/Transdev-Cr92/gtfs-rt -o {output_file}')
+    
+    # if os.path.isfile(f'{cwd}/{output_file}'):
+        
+    with open(f'{cwd}/{output_file}', 'r') as f:
+        j = json.load(f)
+        
+    for n in j['entity']:
+        st.code(n)
+            
+    sleep(20)
+    
+    
